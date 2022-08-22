79537e52860a1513b9a44b19d18f664d5eefd8a4
pbartko <pbartko@netland.com.pl>
2022-08-22 22:28:09 +0200

    add fizzbuzz helper

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace FizzBuzz
 {
@@ -17,32 +15,22 @@ namespace FizzBuzz
 
         public void DisplayFizzBuzz()
         {
-            var elementsList = GenerateList(_startRange, _elementsCount);
+            var elementsList = FizzBuzzHelper.GenerateList(_startRange, _elementsCount);
             foreach (var element in elementsList)
             {
-                if (IsDivisible(element, 3 * 5))
+                if (FizzBuzzHelper.IsDivisible(element, 3 * 5))
                 {
                     Console.WriteLine("FizzBuzz");
                 }
-                else if (IsDivisible(element, 3))
+                else if (FizzBuzzHelper.IsDivisible(element, 3))
                 {
                     Console.WriteLine("Fizz");
                 }
-                else if (IsDivisible(element, 5))
+                else if (FizzBuzzHelper.IsDivisible(element, 5))
                 {
                     Console.WriteLine("Buzz");
                 }
             }
-        }
-
-        private static bool IsDivisible(int dividend, int divisor)
-        {
-            return dividend % divisor == 0;
-        }
-
-        private static IEnumerable<int> GenerateList(int startRange, int elementsCount)
-        {
-            return Enumerable.Range(startRange, elementsCount).ToList();
         }
     }
 }
---
+++ FizzBuzz/FizzBuzz/FizzBuzzHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzzHelper
+    {
+        public static bool IsDivisible(int dividend, int divisor)
+        {
+            return dividend % divisor == 0;
+        }
+
+        public static IEnumerable<int> GenerateList(int startRange, int elementsCount)
+        {
+            return Enumerable.Range(startRange, elementsCount).ToList();
+        }
+    }
+}
